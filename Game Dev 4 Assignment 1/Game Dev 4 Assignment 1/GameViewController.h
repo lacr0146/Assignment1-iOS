3bbe4c26568323c43d55f56b40365db3ef135398
Bryson Lacroix <lacr0146@wj310-19w.ottawa.ad.algonquincollege.com>
2014-01-20 17:03:19 -0500

    Added splash image, fixed a few bugs.

---
+++ Game Dev 4 Assignment 1/Game Dev 4 Assignment 1/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  Game Dev 4 Assignment 1
+//
+//  Created by Bryson Lacroix on 2014-01-20.
+//  Copyright (c) 2014 Bryson Lacroix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewController : UIViewController
+
+-(IBAction)playButtonEvent:(id)sender;
+
+@end
