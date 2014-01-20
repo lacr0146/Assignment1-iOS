0d1fe04b14fa60003f43f0de41d1dc4f547bf931
Bryson Lacroix <lacr0146@wj310-19w.ottawa.ad.algonquincollege.com>
2014-01-20 16:35:55 -0500

    Added load screen
    Added as well an little indicator for "loading"

---
+++ Game Dev 4 Assignment 1/Game Dev 4 Assignment 1/LoadingViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoadingViewController.h
+//  Game Dev 4 Assignment 1
+//
+//  Created by Bryson Lacroix on 2014-01-20.
+//  Copyright (c) 2014 Bryson Lacroix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadingViewController : UIViewController
+
+@end
---
+++ Game Dev 4 Assignment 1/Game Dev 4 Assignment 1/LoadingViewController.m
@@ -0,0 +1,29 @@
+//
+//  LoadingViewController.m
+//  Game Dev 4 Assignment 1
+//
+//  Created by Bryson Lacroix on 2014-01-20.
+//  Copyright (c) 2014 Bryson Lacroix. All rights reserved.
+//
+
+#import "LoadingViewController.h"
+
+@interface LoadingViewController ()
+
+@end
+
+@implementation LoadingViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
